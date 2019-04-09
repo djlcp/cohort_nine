fc72dcd754599dd6fd300d1afe9db207995b41e4
j-vibert <jl_v1991@hotmail.com>
2019-04-09 20:09:09 +0100

    routes prepared for orders view

--- brexit/config/routes.rb
+++ brexit/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :sales
   resources :products
   resources :suppliers
+  resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
