64d3cced7903c601ce79d3c23643c5992cbde51b
Michael Borowski <michael@Michaels-MacBook-Pro.local>
2019-04-09 20:03:30 +0100

    merging

--- brexit/db/schema.rb
+++ brexit/db/schema.rb
@@ -10,22 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_04_09_183240) do
-
-  create_table "sales", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.date "SoldDate"
-    t.integer "quantity"
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 0) do
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 7808edc23355a01709c449be8ce1afde31d813c4
-ActiveRecord::Schema.define(version: 2019_04_09_180911) do
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -33,23 +18,21 @@ ActiveRecord::Schema.define(version: 2019_04_09_180911) do
     t.integer "saleprice"
     t.integer "weight"
     t.string "colour"
->>>>>>> 2e6c1c73a649b5a8116b408d50631ab7886a2480
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
-=======
+  create_table "sales", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.date "SoldDate"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "suppliers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-<<<<<<< HEAD
->>>>>>> bdb662b0324077acd13e4c604a2df65c9ce7bbd3
->>>>>>> 98d226d7ccc982d49a8250fc77c696881fb9e943
 
-=======
->>>>>>> 7808edc23355a01709c449be8ce1afde31d813c4
->>>>>>> 2e6c1c73a649b5a8116b408d50631ab7886a2480
 end
