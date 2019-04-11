019906e8876b65098792fdb5d3ee5607684cd658
Jen <jl_v1991@hotmail.com>
2019-04-11 20:02:17 +0100

    trying to reconcile routing hell

--- brexit/config/routes.rb
+++ brexit/config/routes.rb
@@ -2,11 +2,4 @@ Rails.application.routes.draw do
   resources :sales
   resources :products
   resources :suppliers
-<<<<<<< HEAD
-=======
   resources :orders
-
-  root 'reports#index'
->>>>>>> b9285db3e439075e2a32a51c8f749b8c4afcf7eb
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
