38a880b36c2df293a5cf81284ef49415398df602
Reuben Rushton-Taylor <reubenroy@hotmail.co.uk>
2019-05-07 18:51:54 +0100

    updating branch to development

--- Voucherific/config/routes.rb
+++ Voucherific/config/routes.rb
@@ -18,16 +18,4 @@ Rails.application.routes.draw do
 
   devise_for :users
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-  
-=======
-  root to: "home#index"
-
-  namespace :admin do
-    resources :users, :vouchers, :vendors
-  end
-<<<<<<< HEAD
->>>>>>> 5f0f0d4b313ab64f2fa2eeda759990d84f26f8b2
-=======
->>>>>>> 5f0f0d4b313ab64f2fa2eeda759990d84f26f8b2
 end
