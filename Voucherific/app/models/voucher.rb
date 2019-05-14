d3be4f8b76e5c78e3e68ba7ad8ed48abfd898e9f
Katie <katie_dix@hotmail.co.uk>
2019-05-14 20:00:25 +0100

    merging

--- Voucherific/app/models/voucher.rb
+++ Voucherific/app/models/voucher.rb
@@ -1,9 +1,7 @@
 class Voucher < ApplicationRecord
     has_many :instances
-<<<<<<< HEAD
     has_many :users, through: :instances
-=======
-   has_many :users, through: :instances
+
+    
    has_one :user
->>>>>>> TheTortoise
 end
