2399b33d716ce86f351e2c07d319176399a8bb1b
rr-t <reubenroy@hotmail.co.uk>
2019-05-26 20:13:04 +0100

    List intances working with foreign keys

--- Voucherific/app/controllers/customer/instances_controller.rb
+++ Voucherific/app/controllers/customer/instances_controller.rb
@@ -2,7 +2,10 @@ class Customer::InstancesController < ApplicationController
     #before_action :set_post, only: [:show, :edit, :update, :destroy, :new, :create]
     #load_and_authorize_resource
     def index
-        @instance = Instance.all
+        @instances = Instance.all
+        @voucher = Instance.find_by_id(params[:voucher_id])
+        #@voucher = Voucher.where(id: @voucher_id).first
+        #@voucher = Voucher.where(id: @voucher_id).first
         #@instance.user_id = current_user.id
       end
     
@@ -10,8 +13,9 @@ class Customer::InstancesController < ApplicationController
     # click on a voucher and open for information about specific voucher
     def show
     #@user = User.find(params[:id])
-    @instance.user_id = current_user.id
+    #@instance.user_id = current_user.id
     @instance = Instance.find(params[:id])
+    @voucher = Instance.find_by_id(params[:voucher_id])
     end
 
     # generate new voucher
