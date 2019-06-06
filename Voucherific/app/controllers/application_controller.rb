eefcd8b6f751cce1b9d93a8a53a461f11f0225f0
Jen <jl_v1991@hotmail.com>
2019-06-06 18:50:21 +0100

    Users now redirected to correct landing page, depending on whether they're customer, shop or admin

--- Voucherific/app/controllers/application_controller.rb
+++ Voucherific/app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
 
+	before_action :authenticate_user!
+
+	private
+	def after_sign_in_path_for(resource)
+		if current_user.is_admin?
+			admin_path
+		elsif current_user.is_vendor?
+			shop_path
+		end
+	end
 end
