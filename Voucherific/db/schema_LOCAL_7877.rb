a01eafc2a528d927a3c275d941cac85e8bf0239b
Anca <anca.toma101@gmail.com>
2019-06-13 18:48:16 +0100

    merging??

--- Voucherific/db/schema_LOCAL_7877.rb
+++ Voucherific/db/schema_LOCAL_7877.rb
@@ -10,26 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_173320) do
+ActiveRecord::Schema.define(version: 2019_05_15_211055) do
 
-  create_table "shops", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.integer "contact_phone"
-    t.string "contact_email"
+  create_table "instances", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "voucher_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "templates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.decimal "value", precision: 10
-    t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "is_inactive", default: false
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -40,15 +30,11 @@ ActiveRecord::Schema.define(version: 2019_06_13_173320) do
     t.datetime "dob"
     t.boolean "is_admin"
     t.boolean "is_vendor"
-    t.bigint "shop_id"
-    t.string "first_name"
-    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["shop_id"], name: "index_users_on_shop_id"
   end
 
-  create_table "vendors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "vendors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "location"
     t.integer "contact_phone"
@@ -57,23 +43,16 @@ ActiveRecord::Schema.define(version: 2019_06_13_173320) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "vouchers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "template_id"
+  create_table "vouchers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.decimal "value", precision: 10
+    t.string "description"
+    t.string "type"
+    t.datetime "expiry_date"
+    t.datetime "redeemed_date"
+    t.string "status"
+    t.datetime "payment_sent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "number"
-    t.datetime "redeemed_at"
-    t.datetime "paid_at"
-    t.bigint "customer_id"
-    t.bigint "admin_id"
-    t.bigint "vendor_id"
-    t.index ["admin_id"], name: "index_vouchers_on_admin_id"
-    t.index ["customer_id"], name: "index_vouchers_on_customer_id"
-    t.index ["vendor_id"], name: "index_vouchers_on_vendor_id"
   end
 
-  add_foreign_key "users", "shops"
-  add_foreign_key "vouchers", "users", column: "admin_id"
-  add_foreign_key "vouchers", "users", column: "customer_id"
-  add_foreign_key "vouchers", "users", column: "vendor_id"
 end
