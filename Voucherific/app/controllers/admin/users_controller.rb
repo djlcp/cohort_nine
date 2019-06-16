ab3572f623d372dfc9f273e0c93db305a9963d3b
Jen <jl_v1991@hotmail.com>
2019-06-16 17:55:41 +0100

    admin user controller

--- Voucherific/app/controllers/admin/users_controller.rb
+++ Voucherific/app/controllers/admin/users_controller.rb
@@ -4,26 +4,26 @@ class Admin::UsersController < ApplicationController
 
 	before_action :admin_check
 
-    def index
-        @users = User.all
-    end
+  def index
+    @users = User.all
+  end
 
 
-	def make_vendor
-		
+	def edit
+		  @user = User.find(params[:id])
 	end
 
-  	private  
-  	def user_params
-        params.require(:user).permit(:first_name, :last_name, :is_admin, :is_vendor, :email)
-    end
+	private  
+	def user_params
+      params.require(:user).permit(:first_name, :last_name, :is_admin, :is_vendor, :email)
+  end
 
 
-  	private
-  	def admin_check
-    	if current_user.is_admin? != true
-            redirect_to root_path     
-    	end
+	private
+	def admin_check
+  	if current_user.is_admin? != true
+          redirect_to root_path     
   	end
+	end
 
 end
