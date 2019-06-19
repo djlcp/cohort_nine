dfa5e153fe887082101123eaff4d58bf4c41596e
Jen <jl_v1991@hotmail.com>
2019-06-19 20:35:01 +0100

    added missing parameters to controller

--- Voucherific/app/controllers/registration_controller.rb
+++ Voucherific/app/controllers/registration_controller.rb
@@ -7,6 +7,6 @@ class RegistrationController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :is_admin, :is_vendor, :shop_id)
   end
 end
