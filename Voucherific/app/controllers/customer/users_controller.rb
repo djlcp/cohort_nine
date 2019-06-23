2add93b539ec5633deb2dd1432f825b98acc1f0e
Jen <jl_v1991@hotmail.com>
2019-06-23 14:08:19 +0100

    removing id from editable parameters

--- Voucherific/app/controllers/customer/users_controller.rb
+++ Voucherific/app/controllers/customer/users_controller.rb
@@ -30,7 +30,7 @@ class Customer::UsersController < ApplicationController
 
 	private  
 	def user_params
-      params.require(:user).permit(:id, :first_name, :last_name, :email)
+      params.require(:user).permit(:first_name, :last_name, :email)
   end
 
 end
