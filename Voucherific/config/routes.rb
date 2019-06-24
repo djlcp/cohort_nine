606e40df28d74b52250046e8faf40df7af57ad2f
Jen <jl_v1991@hotmail.com>
2019-06-24 16:39:43 +0100

    unauthenticated users (including after detail change) now redirected to sign-in

--- Voucherific/config/routes.rb
+++ Voucherific/config/routes.rb
@@ -50,6 +50,11 @@ Rails.application.routes.draw do
       resources :vouchers
   end
 
+  devise_scope :user do
+    unauthenticated :user do
+      root :to => 'home#index', as: :unauthenticated_root
+    end
+  end
 
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
