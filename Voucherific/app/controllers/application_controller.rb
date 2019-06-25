84712944504ca20df6906c22e1aecf6d8e0444aa
Matt Chatterley <matt@code.je>
2019-06-25 19:25:01 +0100

    Auto stash before merge of "development" and "origin/development"

--- Voucherific/app/controllers/application_controller.rb
+++ Voucherific/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
+	skip_before_action :verify_authenticity_token
 	before_action :configure_permitted_parameters, if: :devise_controller?
-
+	
 	private
 	def configure_permitted_parameters
     	update_attrs = [:password, :password_confirmation, :current_password]
